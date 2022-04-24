622377dfe439196387f58a6c51645911f920167e
Camilo Chaves <chaves.camilo@gmail.com>
2022-04-24 14:11:50 -0300

    Simple connection completed!

--- RavenConnection/Controllers/APIController.cs
+++ RavenConnection/Controllers/APIController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Raven.Client.Documents.Session;
+using RavenConnection.Database;
+using RavenConnection.Models;
 
 namespace RavenConnection.API
 {
@@ -6,16 +9,58 @@ namespace RavenConnection.API
     [Route("[controller]")]
     public class APIController : ControllerBase
     {
-        public APIController()
+        private readonly IDocumentStoreHolder _db;
+
+        public APIController(IDocumentStoreHolder store)
         {
+            _db = store;
         }
 
         [HttpGet]
         [Route("[action]")]
-        public IActionResult Get()
+        public IActionResult GetAllUsers()
         {
-            var message = "Server is Up!";
-            return Ok(message);
+            using IDocumentSession session = _db.Store.OpenSession();
+            var users = session.Advanced.LoadStartingWith<User>("Users/");
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult GetUserById(string id)
+        {
+            using IDocumentSession session = _db.Store.OpenSession();
+            var user = session.Load<User>(id);
+            if (user is null) return NotFound(new { error= $"user with Id {id} does not exist!"});            
+            return Ok(user);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult CreateUser(User usr)
+        {
+            using IDocumentSession session = _db.Store.OpenSession();
+            session.Store(usr);
+            session.SaveChanges();
+            return Ok(new {User = usr , message = "User stored successfully!"});
+        }
+
+        [HttpPut]
+        [Route("[action]")]
+        public IActionResult UpdateUser(User usr)
+        {
+            using IDocumentSession session = _db.Store.OpenSession();
+            var usrFromDb = session.Load<User>(usr.Id);
+            if (usrFromDb is null) return NotFound(new {error = $"User with Id {usr.Id} not found!"});
+            usrFromDb.Address = usr.Address;
+            usrFromDb.City = usr.City;
+            usrFromDb.Country = usr.Country;
+            usrFromDb.FirstName = usr.FirstName;
+            usrFromDb.LastName = usr.LastName;
+            usrFromDb.Zip = usr.Zip;
+            session.Store(usrFromDb);
+            session.SaveChanges();            
+            return Ok(new {User = usrFromDb , message = "User updated successfully!"});
         }
 
     }
---
+++ RavenConnection/Models/User.cs
@@ -0,0 +1,13 @@
+namespace RavenConnection.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
