78eb73faf15dd064bcddf80326275d400e6188dc
Camilo Chaves <chaves.camilo@gmail.com>
2022-05-05 13:33:13 -0300

    Updated Script

--- RavenConnection/Scripts_Linux_Cli/Start.sh
+++ RavenConnection/Scripts_Linux_Cli/Start.sh
@@ -1,10 +1,20 @@
+#!/bin/bash
 clear
 Raven_BindPort=8080
 Raven_BindTcpPort=38888
+defaultPort=8080
+defaultTcpPort=38888
 
 function StartRaven
 {
-    mode=$1; port=$2; tcpPort=$3; hostname=$4; publicIP=$5
+    mode=$1; port=$2; tcpPort=$3; hostname=$4; publicIP=$5 ; runInCloud=$6
+    if [ $runInCloud == "yes "]; then 
+        RAVEN_URL="RAVEN_PublicServerUrl";
+        RAVEN_TCP="RAVEN_PublicServerUrl_Tcp";
+    else
+        RAVEN_URL="RAVEN_ServerUrl";
+        RAVEN_TCP="RAVEN_ServerUrl_Tcp";
+    fi
     sudo docker kill "raven$node"
     sudo docker container prune --force
     clear
@@ -18,12 +28,12 @@ function StartRaven
             -e RAVEN_Security_UnsecuredAccessAllowed="PublicNetwork" \
             -e RAVEN_BindPort=$Raven_BindPort \
             -e RAVEN_BindTcpPort=$Raven_BindTcpPort \
-            -e RAVEN_PublicServerUrl="http://$publicIp:$Raven_BindPort" \
-            -e RAVEN_PublicServerUrl_Tcp="tcp://$publicIp:$Raven_BindTcpPort" \
+            -e $RAVEN_URL="http://$publicIp:$Raven_BindPort" \
+            -e $RAVEN_TCP="tcp://$publicIp:$Raven_BindTcpPort" \
             -e RAVEN_AUTO_INSTALL_CA="true" \
             -e RAVEN_IN_DOCKER="true" \
             -p $port:$Raven_BindPort -p $tcpPort:$Raven_BindTcpPort  \
-            -h $hostname --network bridge \
+            -h $hostname --network raven_net \
             -v data_$hostname:/opt/RavenDB/Server/RavenData \
             ravendb/ravendb:latest
 }
@@ -51,49 +61,63 @@ function menu
     echo "101. Docker ps -a"
     echo "102. Docker exec <cmd>"
     read -p "Choose an option:"
-    if [ -z ${REPLY} ]; then showMenu=1 ; fi
-    if [ ${REPLY} == 0 ]; then showMenu=0; fi
-    if [ ${REPLY} == 1 ]; then 
-        read -p "Choose the node hostname: "
-        hostname=${REPLY}
-        read -p "what is the node publicIP ? <0.0.0.0 if local> "
-        publicIp=${REPLY}
-        read -p "Choose the Port: "
-        port=${REPLY}
-        read -p "Choose the TCP Port: "
-        tcpPort=${REPLY}
-        StartRaven "None" $port $tcpPort $hostname $publicIp; 
+    option=${REPLY}
+    if [ -z $option ]; then showMenu=1 ; fi
+    if [ $option == 0 ]; then showMenu=0; fi
+    if [ $option == 1 ]; then 
+        read -p "Is container running in cloud? [no] (yes/no) :";
+        if [ -z ${REPLY} ]; then runInCloud="no"; else runInCloud="yes"; fi
+        echo "runInCloud: $runInCloud"
+        read -p "Choose the node hostname: ";
+        hostname=${REPLY};
+        echo "hostname: $hostname";
+        if [ $runInCloud == "yes" ]; then 
+            read -p "what is the node publicIP ?";
+            publicIp=${REPLY};
+        else
+            publicIp=$hostname;
+        fi
+        echo "publicIp=$publicIp";
+        read -p "Choose the Port [$defaultPort] : ";
+        if [ -z ${REPLY} ] || [ "${REPLY}" == "no" ]; then port=$defaultPort; else port=${REPLY}; fi
+        echo "port=$port"
+        read -p "Choose the TCP Port [$defaultTcpPort] : ";
+        if [ -z ${REPLY} ]; then tcpPort=$defaultTcpPort; else tcpPort=${REPLY}; fi
+        echo "tcpPort=$tcpPort"
+        read -p "press any key to launch container..."
+        StartRaven "None" $port $tcpPort $hostname $publicIp $runInCloud; 
     fi  
-    if [ ${REPLY} == 2 ]; then
+    if [ $option == 2 ]; then
         for container in $(sudo docker ps -q -f name=raven)
         do
-            sudo docker kill $container
+            sudo docker kill $container;
         done
-        sudo docker container prune --force
-        sudo docker ps -a
+        sudo docker container prune --force;
+        sudo docker ps -a;
     fi
-    if [ ${REPLY} == 3 ]; then
-        read -p "Kill which container hostname ? "
-        sudo docker kill ${REPLY}
-        sudo docker container prune --force
+    if [ $option == 3 ]; then
+        read -p "Kill which container hostname ? ";
+        sudo docker kill ${REPLY};
+        sudo docker container prune --force;
     fi
-    if [ ${REPLY} == 4 ]; then
-        read -p "Master Node hostname or IP: "
-        master=${REPLY}
-        read -p "Slave Node hostname or IP: "
-        slave=${REPLY}
-        AddNodeToCluster $master $slave        
+    if [ $option == 4 ]; then
+        read -p "Master Node container name: ";
+        master=${REPLY};
+        read -p "Slave Node container name(if local) or PublicIp: ";
+        slave=${REPLY};
+        AddNodeToCluster $master $slave;   
     fi
-    if [ ${REPLY} == 100 ]; then
-        read -p "Which Node hostname ? " 
-        sudo docker logs ${REPLY}; 
+    if [ $option == 100 ]; then
+        read -p "Which Node hostname ? ";
+        sudo docker logs ${REPLY};
     fi
-    if [ ${REPLY} == 101 ]; then sudo docker ps -a; fi
-    if [ ${REPLY} == 102 ]; then
-        read -p "Which Node hostname ? "
-        node = ${REPLY} 
-        read -p "Command: " 
-        sudo docker exec $node ${REPLY};
+    if [ $option == 101 ]; then sudo docker ps -a; fi
+    if [ $option == 102 ]; then
+        read -p "Which Node hostname ? ";
+        hostname=${REPLY} ;
+        read -p "Command: " ;
+        command=${REPLY};
+        sudo docker exec -it $hostname $command;
     fi 
 }
 
