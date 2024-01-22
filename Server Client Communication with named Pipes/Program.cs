c34864a157eca002e28290b6912a4aea4fd22c44
Camilo Chaves <chaves.camilo@gmail.com>
2024-01-22 14:17:19 -0300

    Upgrading PIPE Example

--- Server Client Communication with named Pipes/Program.cs
+++ Server Client Communication with named Pipes/Program.cs
@@ -10,60 +10,59 @@ using System.Text;
 using static System.Console;
 using System.Threading.Tasks;
 
-namespace Server_Client_Communication_with_named_Pipes
+class Program
 {
-	public class Program
+	static void Main(string[] args)
 	{
-		public static void Main()
+		WriteLine("Pipes communication example");
+		Task.Run(() => PipeServer("PipeOne"));
+		Task.Run(() => PipeClient("127.0.0.1", "PipeOne"));
+		//Task.Run(() => PipeClient("127.0.0.1", "PipeOne"));
+		ReadLine();
+	}
+
+	static void PipeServer(string pipeName)
+	{
+		WriteLine($"Server Pipe name: {pipeName}");
+		try
 		{
-			WriteLine("Pipes communication example");
-			Task.Run(() => PipeReader("PipeOne"));
-			Task.Run(() => PipeWrite("localhost", "PipeOne"));
+			using var pipeReader = new NamedPipeServerStream(pipeName, PipeDirection.In);
+			pipeReader.WaitForConnection();
+			WriteLine("Reader connected!");
+			const int BUFFERSIZE = 256;
+			bool terminated = false;
+			while (!terminated)
+			{
+				byte[] buffer = new byte[BUFFERSIZE];
+				int nRead = pipeReader.Read(buffer, 0, BUFFERSIZE);
+				string line = Encoding.UTF8.GetString(buffer, 0, nRead);
+				WriteLine(line);
+				if (line == "Bye") terminated = true;
+			}
+			WriteLine("Channel closed!");
 			ReadLine();
 		}
+		catch (Exception ex) { WriteLine(ex.Message); }
+	}
 
-		private static void PipeReader(string pipeName)
+	static void PipeClient(string serverName, string pipeName)
+	{
+		WriteLine($"Client:  Server name: {serverName} Pipe:{pipeName}");
+		try
 		{
-			WriteLine($"Pipe Server name: {pipeName}");
-			try
-			{
-				using var pipeReader = new NamedPipeServerStream(pipeName, PipeDirection.In);
-				pipeReader.WaitForConnection();
-				WriteLine("Reader connected!");
-				const int BUFFERSIZE = 256;
-				bool terminated = false;
-				while (!terminated)
-				{
-					byte[] buffer = new byte[BUFFERSIZE];
-					int nRead = pipeReader.Read(buffer, 0, BUFFERSIZE);
-					string line = Encoding.UTF8.GetString(buffer, 0, nRead);
-					WriteLine(line);
-					if (line == "Bye") terminated = true;
-				}
-				WriteLine("Channel closed!");
-				ReadLine();
-			}
-			catch (Exception ex) { WriteLine(ex.Message); }
+			using var pipeWriter = new NamedPipeClientStream(serverName, pipeName, PipeDirection.Out);
+			pipeWriter.Connect();
+			WriteLine("Writer connected!");
+			string input = "This is a message sent through a Pipe";
+			byte[] buffer = Encoding.UTF8.GetBytes(input);
+			pipeWriter.Write(buffer, 0, buffer.Length);
+			input = "Bye";
+			buffer = Encoding.UTF8.GetBytes(input);
+			pipeWriter.Write(buffer, 0, buffer.Length);
+			WriteLine("Messaged terminated!");
 		}
-
-		private static void PipeWrite(string serverName, string pipeName)
-		{
-			WriteLine($"Pipe Client name: {pipeName}");
-			try
-			{
-				using var pipeWriter = new NamedPipeClientStream(serverName, pipeName, PipeDirection.Out);
-				pipeWriter.Connect();
-				WriteLine("Writer connected!");
-				string input = "This is a message sent through a Pipe";
-				byte[] buffer = Encoding.UTF8.GetBytes(input);
-				pipeWriter.Write(buffer, 0, buffer.Length);
-				input = "Bye";
-				buffer = Encoding.UTF8.GetBytes(input);
-				pipeWriter.Write(buffer, 0, buffer.Length);
-				WriteLine("Messaged terminated!");
-			}
-			catch (Exception ex) { WriteLine(ex.Message); }
-		}
-
+		catch (Exception ex) { WriteLine(ex.Message); }
 	}
 }
+
+
