8bd3ab032cff8c6ffdb999b210085f1c0f5f9a20
Darth Linuxer <camilo.chaves@capgemini.com>
2024-01-25 15:22:40 -0300

    Updated

--- Server Client Communication with named Pipes/obj/Debug/.net8/Server Client Communication with named Pipes.AssemblyInfo.cs
+++ Server Client Communication with named Pipes/obj/Debug/.net8/Server Client Communication with named Pipes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server Client Communication with named Pipes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c34864a157eca002e28290b6912a4aea4fd22c44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dfe3c846d9b746cc8e0f0f480f15dd6fb927440")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server Client Communication with named Pipes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server Client Communication with named Pipes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
